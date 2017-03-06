128eb2008f4ef6539fba0f173d3761ef99af1c58
USER <USER@192.168.1.252>
2017-03-06 18:22:31 +0800

    2017/0111

--- Myjava04/src/tw/matt01/MTJAVA01.java
+++ Myjava04/src/tw/matt01/MTJAVA01.java
@@ -3,7 +3,8 @@ package tw.matt01;
 public class MTJAVA01 {
 
 	public static void main(String[] args) {
-      System.out.println(123);
+		System.out.println(123);
+		System.out.println(456);
 
 	}
 
